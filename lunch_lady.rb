6607357208dc8f550ca2ef4cde5e07fa843623ea
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-05-28 16:38:03 -0600

    created method to print out order, prices, and total

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -13,7 +13,7 @@ class Lunchlady
              "Guac " => 1.00,
              "Beans" => 1.50,
              "Rice " => 1.50,
-             "Weird soup" => 2.00,
+             "Soup " => 2.00,
              "Salad" => 2.50
            }
 
@@ -42,8 +42,18 @@ class Lunchlady
     end
     print "Por favor, elija el número de plato: "
     @side_dish_choice = gets.chomp.to_i
-    puts "¿Qué te gustaría para tu segundo plato?"
+    print "¿Qué te gustaría para tu segundo plato?: "
     @side_dish_choice_two = gets.chomp.to_i
+  end
+
+  def ring_up_order
+    puts "#{@@main_dishes.keys[@main_dish_choice]}\t$#{sprintf('%.2f',@@main_dishes.values[@main_dish_choice])} "
+    puts "#{@@sides.keys[@side_dish_choice]}\t\t$#{sprintf('%.2f',@@sides.values[@side_dish_choice])}"
+    puts "#{@@sides.keys[@side_dish_choice_two]}\t\t$#{sprintf('%.2f',@@sides.values[@side_dish_choice_two])}"
+    puts "-"*22
+    print "\t\t"
+    puts "$#{sprintf('%.2f',@@main_dishes.values[@main_dish_choice] + @@sides.values[@side_dish_choice] + @@sides.values[@side_dish_choice_two])}"
+
   end
 
 end
@@ -51,4 +61,5 @@ end
 l = Lunchlady.new
 l.pick_a_main_dish
 l.pick_a_side_dish
+l.ring_up_order
 
