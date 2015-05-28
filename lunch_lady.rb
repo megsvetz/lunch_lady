0d4b6819f6e971cc4a4823a2853825516820fbc9
Meg Svetz <megsvetz@gmail.com>
2015-05-28 15:49:54 -0600

    added main_dish method with menu

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,3 +1,14 @@
 class Lunchlady
-#code goes here
+
+  def main_dish
+    main_dishes = {"taco" => 1.50,
+                   "burrito" => 4.00,
+                   "nachos" => 3.00,
+                   "ultra burrito" => 6.00,
+                   "super nachos" => 5.00,
+                   "chips and salsa" => 2.00
+                  }
+  end
+
 end
+
