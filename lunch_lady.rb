cff4343fb2c41df6b04ced0011023a4b6f4dd57c
Meg Svetz <megsvetz@gmail.com>
2015-05-28 16:26:35 -0600

    added pick_a_side_dish

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -10,14 +10,12 @@ class Lunchlady
                   }
 
     @@sides = {"Chips" => 1.50,
-             "Guac" => 1.00,
+             "Guac " => 1.00,
              "Beans" => 1.50,
-             "Rice" => 1.50,
+             "Rice " => 1.50,
              "Weird soup" => 2.00,
              "Salad" => 2.50
            }
-    
-
 
   def pick_a_main_dish
     puts "Hola, bienvenido a delicious palacio alimentos!"
@@ -30,10 +28,27 @@ class Lunchlady
 
     end
     print "Por favor, elija el número de plato: "
-    main_dish_choice = gets.chomp.to_i
+    @main_dish_choice = gets.chomp.to_i
   end
+
+  def pick_a_side_dish
+    puts "¿Qué te gustaría para tu plato?"
+
+    z = 1
+    @@sides.each do |x, y|
+      puts "#{z}. #{x}\t\t$#{sprintf('%.2f', y)}"
+      z+=1
+
+    end
+    print "Por favor, elija el número de plato: "
+    @side_dish_choice = gets.chomp.to_i
+    puts "¿Qué te gustaría para tu segundo plato?"
+    @side_dish_choice_two = gets.chomp.to_i
+  end
+
 end
 
 l = Lunchlady.new
 l.pick_a_main_dish
+l.pick_a_side_dish
 
