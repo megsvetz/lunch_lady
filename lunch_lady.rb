216181fdc4ba8b872676b45b0b19408ed71c44e8
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-05-28 15:54:06 -0600

    added side dished, removed something from main dishes

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,13 +1,21 @@
 class Lunchlady
 
-  def main_dish
-    main_dishes = {"taco" => 1.50,
-                   "burrito" => 4.00,
-                   "nachos" => 3.00,
-                   "ultra burrito" => 6.00,
-                   "super nachos" => 5.00,
-                   "chips and salsa" => 2.00
+  def menu
+    main_dishes = {"Taco" => 1.50,
+                   "Burrito" => 4.00,
+                   "Nachos" => 3.00,
+                   "Ultra Burrito" => 6.00,
+                   "Super Nachos" => 5.00,
+                   "Quesadilla" => 3.50
                   }
+
+    sides = {"Chips" => 1.50,
+             "Guac" => 1.00,
+             "Beans" => 1.50,
+             "Rice" => 1.50,
+             "Weird soup" => 2.00,
+             "Salad" => 2.50
+    }
   end
 
 end
