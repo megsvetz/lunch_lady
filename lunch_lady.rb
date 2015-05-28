24d7d25f8f65a4015bd1cbdea792d720ff898e7c
Meg Svetz <megsvetz@gmail.com>
2015-05-28 16:49:01 -0600

    added in sleeps and puts for easier readability

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -18,8 +18,13 @@ class Lunchlady
            }
 
   def pick_a_main_dish
+    puts ""
     puts "Hola, bienvenido a delicious palacio alimentos!"
+    sleep 1
+    puts ""
     puts "¿Qué te gustaría para su plato principal?"
+    puts ""
+    sleep 1
 
     z = 1
     @@main_dishes.each do |x, y|
@@ -27,12 +32,19 @@ class Lunchlady
       z+=1
 
     end
+    puts ""
+    sleep 1
     print "Por favor, elija el número de plato: "
     @main_dish_choice = gets.chomp.to_i
+    @main_dish_choice -= 1
   end
 
   def pick_a_side_dish
+    puts ""
+    sleep 1
     puts "¿Qué te gustaría para tu plato?"
+    sleep 1
+    puts ""
 
     z = 1
     @@sides.each do |x, y|
@@ -40,20 +52,32 @@ class Lunchlady
       z+=1
 
     end
+    puts ""
     print "Por favor, elija el número de plato: "
     @side_dish_choice = gets.chomp.to_i
+    @side_dish_choice -= 1 
+    sleep 1
+    puts ""
     print "¿Qué te gustaría para tu segundo plato?: "
     @side_dish_choice_two = gets.chomp.to_i
+    @side_dish_choice_two -= 1
   end
 
   def ring_up_order
-    puts "#{@@main_dishes.keys[@main_dish_choice]}\t$#{sprintf('%.2f',@@main_dishes.values[@main_dish_choice])} "
+    puts ""
+    sleep 1
+    puts "Gracias por venir!"
+    sleep 1
+    puts "Aquí está su cuenta:"
+    sleep 1
+    puts ""
+    puts "#{@@main_dishes.keys[@main_dish_choice]}\t\t$#{sprintf('%.2f',@@main_dishes.values[@main_dish_choice])} "
     puts "#{@@sides.keys[@side_dish_choice]}\t\t$#{sprintf('%.2f',@@sides.values[@side_dish_choice])}"
     puts "#{@@sides.keys[@side_dish_choice_two]}\t\t$#{sprintf('%.2f',@@sides.values[@side_dish_choice_two])}"
     puts "-"*22
-    print "\t\t"
+    print "Total\t\t"
     puts "$#{sprintf('%.2f',@@main_dishes.values[@main_dish_choice] + @@sides.values[@side_dish_choice] + @@sides.values[@side_dish_choice_two])}"
-
+    puts ""
   end
 
 end
